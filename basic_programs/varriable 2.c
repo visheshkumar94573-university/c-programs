cd7b59750896d52002e1f6e6bdf20d018035c624
vishesh kumar <visheshkumar86973@gmail.com>
2026-06-10 12:12:46 +0530

    Add files via upload

---
+++ basic_programs/varriable 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a = 2;
+    int b = 5;
+    printf("%d\n",a*b);
+    int  x = 4;
+    int  y = 7;
+    int  z = x + y;
+    printf("%d\n",z);
+
+    return 0;
+}
---
+++ basic_programs/varriable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    x = 3;
+    printf("%d",x);
+    x = x + 5;
+    printf("%d",x);
+    return 0;
+}
